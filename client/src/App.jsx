4b22bc0b4a0e6cfc25e25dfda5d8d76c4a983fe5
Baldeep Singh <itsbaldeep@gmail.com>
2021-10-15 18:39:02 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,40 @@
+// Libraries
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// Components
+import Navbar from "./components/Navbar.jsx";
+
+// Screens
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+// Theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ab47bc",
+    },
+    secondary: {
+      main: "#ab47bc",
+    },
+  },
+});
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
