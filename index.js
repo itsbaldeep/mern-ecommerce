ff764131f8068bc6f33593a674791104812b9cbd
Baldeep Singh <itsbaldeep@gmail.com>
2021-10-31 12:08:10 +0530

    Added favicon and production env file for react

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ app.use("/api/service", require("./routes/service"));
 // Middlewares
 app.use(require("./middleware/errorHandler"));
 
+app.use("/", express.static(path.join(__dirname, "./client/build")));
 // Serving static frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.use("/", express.static(path.join(__dirname, "./client/build")));
+  app.use("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
